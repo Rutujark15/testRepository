51b7380c51fb70117e951857797af043b10fa1fd
Rutujark15 <rutuja-rajiv.kulkarni@capgemini.com>
2025-07-08 13:59:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = AKIAQNGLNHNF74TY6TQH
+  secret_key = gcoF4YzSII7XrYyHRpdEK3Jhm+C93u+zvgBpZ/q5y
 }
 resource "aws_instance" "example" {
   ami           = var.ami
