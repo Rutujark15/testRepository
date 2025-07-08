2770a2bdf75eb87db054c42fde30e2c4fc06a4ff
Rutujark15 <rutuja-rajiv.kulkarni@capgemini.com>
2025-07-08 13:09:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,3 +10,11 @@ variable "ami" {
   description = "AMI ID for the EC2 instance"
   type        = string
 }
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
